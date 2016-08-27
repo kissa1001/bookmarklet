337a170f3d11e83c4ae3ad035274be4f42914385
kissa1001 <kissa101295@gmail.com>
2016-08-26 20:53:47 -0400

    improved style

--- app.js
+++ app.js
@@ -19,19 +19,20 @@ function cloudWords(string) {
   return array;
 }
 function sortBySize(words){
-  var max = 10;
+  var max = 20;
   var min = 1;
-  var fontMin = 10;
-  var fontMax = 20;
+  var fontMin = 40;
+  var fontMax = 70;
   for (var i in words){
     var word = words[i];
     var size = word.Uses == min ? fontMin     : (word.Uses / max) * (fontMax - fontMin) + fontMin;
-    var paragraph = document.createElement("p");
-    paragraph.style.fontSize = size + "px";
-    paragraph.style.display = "inline";
-    paragraph.innerHTML = word.Name + " ";
-    document.body.appendChild(paragraph);
+    document.body.style.fontSize = size + "px";
+    document.body.style.display = "inline";
+    document.body.style.color = "#fff";
+    document.body.style.backgroundColor = "#444";
+    var paragraph = word.Name + " ";
+    document.write(paragraph);
   }
 }
 sortBySize(cloudWords(string));
-});
+})();
